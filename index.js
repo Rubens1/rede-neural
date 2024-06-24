b7696f3606d6a968906651c2d8ca40c8916eefb6
Rubens Nogueira <rubens.jesus1997@gmail.com>
2024-06-24 16:40:24 -0300

    A5FAA002599228BCFA14BACAF587C3C5FACDC60798DBA44DE553BE740ED48466690FBBF71DC5EBFCE551C3B293777DB68445F7F5CBA9BEFDF9266667A0BB224B
    A5FAA002599228BCFA14BACAF587C3C5FACDC60798DBA44DE553BE740ED48466690FBBF71DC5EBFCE551C3B293777DB68445F7F5CBA9BEFDF9266667A0BB224B

---
+++ index.js
@@ -0,0 +1,45 @@
+const synaptic = require('synaptic');
+
+
+
+// Criar as camadas
+let inputLayer = new synaptic.Layer(2);
+let hiddenLayer = new synaptic.Layer(3);
+let outputLayer = new synaptic.Layer(1);
+
+// Projetar as camadas
+inputLayer.project(hiddenLayer);
+hiddenLayer.project(outputLayer);
+
+// Criar a rede
+let myNetwork = new synaptic.Network({
+  input: inputLayer,
+  hidden: [hiddenLayer],
+  output: outputLayer
+});
+
+// Definir a taxa de aprendizado
+let learningRate = 0.3;
+
+// Treinar a rede
+for (let i = 0; i < 20000; i++) {
+  // 0 xor 0 => 0
+  myNetwork.activate([0, 0]);
+  myNetwork.propagate(learningRate, [0]);
+  // 0 xor 1 => 1
+  myNetwork.activate([0, 1]);
+  myNetwork.propagate(learningRate, [1]);
+  // 1 xor 0 => 1
+  myNetwork.activate([1, 0]);
+  myNetwork.propagate(learningRate, [1]);
+  // 1 xor 1 => 0
+  myNetwork.activate([1, 1]);
+  myNetwork.propagate(learningRate, [0]);
+}
+
+console.log("============================= Início =============================");
+console.log(myNetwork.activate([0, 0])); // ~0
+console.log(myNetwork.activate([0, 1])); // ~1
+console.log(myNetwork.activate([1, 0])); // ~1
+console.log(myNetwork.activate([1, 1])); // ~0
+console.log("============================= Fim =============================");
